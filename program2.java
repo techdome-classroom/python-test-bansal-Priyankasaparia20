0129f60da1bdc0cf0a1a6810947dc90b22c2b621
Priyankasaparia20 <126343133+Priyankasaparia20@users.noreply.github.com>
2024-02-23 13:14:53 +0530

    Update program2.java

--- program2.java
+++ program2.java
@@ -1,17 +1,22 @@
-public int romanToInt(String s) {
-         int ans = 0, num = 0;
-        for (int i = s.length()-1; i >= 0; i--) {
-            switch(s.charAt(i)) {
-                case 'I': num = 1; break;
-                case 'V': num = 5; break;
-                case 'X': num = 10; break;
-                case 'L': num = 50; break;
-                case 'C': num = 100; break;
-                case 'D': num = 500; break;
-                case 'M': num = 1000; break;
-            }
-            if (4 * num < ans) ans -= num;
-            else ans += num;
+class Solution {
+    public static int romanToInt(String s) {
+        if (s == null || s.length() == 0)
+            return -1;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int len = s.length(), result = map.get(s.charAt(len - 1));
+        for (int i = len - 2; i >= 0; i--) {
+            if (map.get(s.charAt(i)) >= map.get(s.charAt(i + 1)))
+                result += map.get(s.charAt(i));
+            else
+                result -= map.get(s.charAt(i));
         }
-        return ans;
+        return result;
     }
+}
